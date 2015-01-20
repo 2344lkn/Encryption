8d45fb83c70b239543fbd1910197c9a9dd128d1d
2344lkn <2344lkn@gmail.com>
2015-01-20 12:35:52 -0800

    Create salt.cs

---
+++ Files/fileEncrypt/Encryption/salt.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace fileEncrypt.Encryption
+{
+  class salt
+  {
+    public static string Encode() {}
+    public static string Decode() {}
+  }
+}

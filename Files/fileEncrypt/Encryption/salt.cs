b6262253da91da8ace419b7ba4191789c71157ea
2344lkn <2344lkn@gmail.com>
2015-03-05 16:10:41 -0800

    Update salt.cs

--- Files/fileEncrypt/Encryption/salt.cs
+++ Files/fileEncrypt/Encryption/salt.cs
@@ -6,11 +6,62 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
-namespace fileEncrypt.Encryption
+namespace encryptConcept.Encryption
 {
-  class salt
-  {
-    public static string Encode() {}
-    public static string Decode() {}
-  }
+    class salt
+    {
+        public static string Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Decode(string encodedData)
+        {
+            var encodedBytes = System.Convert.FromBase64String(encodedData);
+            return System.Text.Encoding.UTF8.GetString(encodedBytes);
+        }
+
+        private const int PBKDF2IterCount = 1000;       // default derive bytes
+        private const int PBKDF2SubkeyLength = 256 / 8; // 256bit
+        private const int SaltSize = 128 / 8;           // 128bit
+
+        public static string hashPassword(string password)
+        {
+            byte[] salt;
+            byte[] subkey;
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, SaltSize, PBKDF2IterCount))
+            {
+                salt = deriveBytes.Salt;
+                subkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+            }
+
+            byte[] outputBytes = new byte[1 + SaltSize + PBKDF2SubkeyLength];
+            Buffer.BlockCopy(salt, 0, outputBytes, 1, SaltSize);
+            Buffer.BlockCopy(subkey, 0, outputBytes, 1 + SaltSize, PBKDF2SubkeyLength);
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        public static bool VerifyHashedPassword(string hashedPassword, string password)
+        {
+            byte[] hashedPasswordBytes = Convert.FromBase64String(hashedPassword);
+
+            // Wrong length or version header
+            if (hashedPasswordBytes.Length != (1 + SaltSize + PBKDF2SubkeyLength) || hashedPasswordBytes[0] != 0x00)
+                return false;
+
+            byte[] salt = new byte[SaltSize];
+            Buffer.BlockCopy(hashedPasswordBytes, 1, salt, 0, SaltSize);
+            byte[] storedSubkey = new byte[PBKDF2SubkeyLength];
+            Buffer.BlockCopy(hashedPasswordBytes, 1 + SaltSize, storedSubkey, 0, PBKDF2SubkeyLength);
+
+            byte[] generatedSubkey;
+            using (var deriveBytes = new Rfc2898DeriveBytes(password, salt, PBKDF2IterCount))
+            {
+                generatedSubkey = deriveBytes.GetBytes(PBKDF2SubkeyLength);
+            }
+            Console.WriteLine(storedSubkey.SequenceEqual(generatedSubkey));
+            return storedSubkey.SequenceEqual(generatedSubkey);
+        }
+    }
 }
